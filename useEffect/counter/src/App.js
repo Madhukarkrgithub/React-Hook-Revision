8a34216ba9974cdd33c0f1830511b8683c86cd39
Madhukar kumar <madhukarkumar2468@gmail.com>
2024-03-14 23:09:26 +0530

    code

--- useEffect/counter/src/App.js
+++ useEffect/counter/src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
+import Count from './Count';
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>Counter App</h1>
+      <Count/>
     </div>
   );
 }
